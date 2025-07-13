7700344cd0babe0d56c0aa473cc803a5b844b4a0
sohelllkhan <sohelkhan323526@gmail.com>
2025-07-13 16:09:09 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(cors({
+  origin: 'https://sohelllkhan.github.io/my-project/', // or '*'
+}));
 app.use(express.json());
 
 // MongoDB connection
