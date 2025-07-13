87342686394ff67a89c5fd69a22d612e8fb93143
sohelllkhan <sohelkhan323526@gmail.com>
2025-07-13 16:51:22 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://sohelllkhan.github.io/my-project/', // or '*'
+  origin: '*'
 }));
 app.use(express.json());
 
